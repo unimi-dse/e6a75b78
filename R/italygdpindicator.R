f2c373590dc56591e1d72b3ee3d4fd92ba2fb4c3
akubualexandrew <baakalex9@gmail.com>
2020-02-15 23:39:18 +0300

    plot function

--- R/italygdpindicator.R
+++ R/italygdpindicator.R
@@ -1,27 +1,23 @@
 
 
 #import data----
-library(ggplot2)
+
 sf<-system.file("extdata","IMEXP.csv",package = "mypackage")
 print(sf)
 data<-read.csv(sf)
 print(data)
 
 year<-c(2000:2018)
-# importdata <-data[1,][4:22]
+
+#Import
 importdata<- c(as.numeric(data[data[,"Indicator"]== "import" ,][4:22]))
 italyimports<-data.frame(year,importdata)
-# ggplot(italyimports, aes('wt', 'mpg')) + geom_line()
-plot(italyimports)
+p1 <- ggplot2::ggplot(italyimports, ggplot2::aes(year, importdata) ) + ggplot2::geom_line()
+p1
 
-
-
-
-print(years)
-print(data)
-year<-c(2000:2018)
-importdata <-data[2,][4:22]
-italyexports<-data.frame(year,importdata)
-importdata<- c(as.numeric(data[data[,"indicator"]=="export"][4:22]))
-plot(italyexports)
+#Export
+exportdata<- c(as.numeric(data[data[,"Indicator"]=="export",][4:22]))
+italyexports<-data.frame(year,exportdata)
+p2 <- ggplot2::ggplot(italyimports, ggplot2::aes(year, importdata) ) + ggplot2::geom_line()
+p2
 
