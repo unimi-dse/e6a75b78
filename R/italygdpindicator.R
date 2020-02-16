85fd8ae3302b35093c4b12ab1cb0db27572bc573
akubualexandrew <baakalex9@gmail.com>
2020-02-17 00:52:50 +0300

    document function

--- R/italygdpindicator.R
+++ R/italygdpindicator.R
@@ -1,23 +1,29 @@
+#' @title GDP contribution between imports and exports
+#' @description relationship between italys imports and exports on gdp, line chart decribes the graphical relationship for the period 2000 to 2018
+#' @details transform all block letters to small letters
+#'
+#' @export
 
 
-#import data----
+italygdp <- function(){
+  #import data----
 
-sf<-system.file("extdata","IMEXP.csv",package = "mypackage")
-print(sf)
-data<-read.csv(sf)
-print(data)
 
-year<-c(2000:2018)
+  sf<-system.file("extdata","IMEXP.csv",package = "mypackage")
+  data<-read.csv(sf)
 
-#Import
-importdata<- c(as.numeric(data[data[,"Indicator"]== "import" ,][4:22]))
-italyimports<-data.frame(year,importdata)
-p1 <- ggplot2::ggplot(italyimports, ggplot2::aes(year, importdata) ) + ggplot2::geom_line()
-p1
+  year<-c(2000:2018)
 
-#Export
-exportdata<- c(as.numeric(data[data[,"Indicator"]=="export",][4:22]))
-italyexports<-data.frame(year,exportdata)
-p2 <- ggplot2::ggplot(italyimports, ggplot2::aes(year, importdata) ) + ggplot2::geom_line()
-p2
+  #Import
+  importdata<- c(as.numeric(data[data[,"Indicator"]== "import" ,][4:22]))
+  italyimports<-data.frame(year,importdata)
+  p1 <- ggplot2::ggplot(italyimports, ggplot2::aes(year, importdata) ) + ggplot2::geom_line()
+  p1
 
+  #Export
+  exportdata<- c(as.numeric(data[data[,"Indicator"]=="export",][4:22]))
+  italyexports<-data.frame(year,exportdata)
+  p2 <- ggplot2::ggplot(italyexports, ggplot2::aes(year, exportdata) ) + ggplot2::geom_line()
+  p2
+
+}
--- README.md
+++ README.md
@@ -1,1 +1,35 @@
-# e6a75b78
+# ITALY IMPORTS AND EXPORTS
+`mypackage` describes the relationship between the imports and exports of Italy on GDP% contribution for the years 2000 to 2018. `mypackage` is a time series as it helps to describe the relationship between the variables imports and exports in relation to GDP.
+
+## Installation
+
+```bash
+# first install the R package "devtools" if not installed
+devtools::install_github('unimi-dse/e6a75b78')
+```
+## Usage
+
+```r
+# load the package
+require(mypackage)
+```
+
+### Run app
+`italygdp()`
+
+## Dataset
+The data set was downloaded from Data World Bank website [Data World Bank](https://data.worldbank.org/) and it describes the economic GDP contribution of Italy's imports and exports. keep in mind the data was downloaded as a CSV file. 
+
+## Contributions
+My project is open for any corrections and feel free to make suggestions and any improvements where necessary.
+
+## Technologies
+used R studio
+
+## Data visual
+Use install package `ggplot2`, helps describe the graphical relation between different variables.
+
+## License
+GPL-1
+## Author
+Alex Andrew Akubu
